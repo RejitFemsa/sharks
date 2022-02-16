445decc86ecc2fb01499fe11d71af1a9387e9f69
RejitFemsa <juan.fuentes@oxxo.com>
2022-02-16 13:50:13 -0600

    dockerized and running

---
+++ entrypoints/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
+
+bundle install
+
+bundle update
+
+bundle exec rails s -b 0.0.0.0
---
+++ entrypoints/sidekiq-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
+
+bundle install
+
+bundle update
+
+bundle exec sidekiq
